0a9c45ac1ceb1acd53e0c7ff0ad128b3a0f0f4a5
liuyk <271007729@qq.com>
2018-12-04 09:49:02 +0800

    修改成功返回code值，兼容现有框架

--- commons-core/src/main/java/com/itxiaoer/commons/core/page/ResponseCode.java
+++ commons-core/src/main/java/com/itxiaoer/commons/core/page/ResponseCode.java
@@ -5,7 +5,7 @@ package com.itxiaoer.commons.core.page;
  */
 @SuppressWarnings({"unused", "WeakerAccess"})
 public final class ResponseCode {
-    public final static String SUCCESS_CODE = "200";
+    public final static String SUCCESS_CODE = "0";
     public final static String NO_PERMISSION = "40001";
     public final static String PARAMETER_VALID_CODE = "40002";
     public final static String LOGIN_PASSWORD_ERROR_CODE = "40003";
