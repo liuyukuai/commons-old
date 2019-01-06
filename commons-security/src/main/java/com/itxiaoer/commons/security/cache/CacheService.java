cae11e9482aa4a1cce5f734667e84b6ab4ba2474
liuyk <271007729@qq.com>
2019-01-06 19:16:22 +0800

    去掉警告

--- commons-security/src/main/java/com/itxiaoer/commons/security/cache/CacheService.java
+++ commons-security/src/main/java/com/itxiaoer/commons/security/cache/CacheService.java
@@ -7,13 +7,39 @@ import java.util.Date;
 /**
  * @author : liuyk
  */
+@SuppressWarnings("UnusedReturnValue")
 public interface CacheService {
-
+    /**
+     * set key
+     *
+     * @param key   key
+     * @param token token
+     * @return boolean
+     */
     Boolean setIfAbsent(String key, JwtToken token);
 
+    /**
+     * get token
+     *
+     * @param key key
+     * @return token
+     */
     JwtToken get(String key);
 
+    /**
+     * set token
+     *
+     * @param key   key
+     * @param token token
+     */
     void set(String key, JwtToken token);
 
+    /**
+     * set token
+     *
+     * @param key  key
+     * @param date date
+     * @return boolean
+     */
     Boolean expireAt(String key, Date date);
 }
