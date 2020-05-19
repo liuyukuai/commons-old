4396119a108d6d7ebc0caa97a9eb7795bd207486
liuyk <271007729@qq.com>
2020-05-19 18:10:02 +0800

    增加方法

--- commons-jpa/src/main/java/com/itxiaoer/commons/jpa/service/BasicJpaService.java
+++ commons-jpa/src/main/java/com/itxiaoer/commons/jpa/service/BasicJpaService.java
@@ -13,12 +13,9 @@ import com.itxiaoer.commons.orm.service.BasicSpecificationExecutor;
 import com.itxiaoer.commons.orm.validate.Validate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.CollectionUtils;
 
 import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
